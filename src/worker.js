80add3cc142ccc260417cd4f150d3fb09f29dfd6
Max <tutv95@gmail.com>
2019-04-04 11:55:35 +0700

    test

--- src/worker.js
+++ src/worker.js
@@ -1,12 +1,21 @@
 const Schedule = require('node-schedule')
 const {EVERY_MINUTE} = require('./constants/Crontab')
 
+const _getCurrentContest = () => {
+    const repo = process.env.GITHUB_REPO || 'tets-whois'
+    const owner = process.env.GITHUB_OWNER || 'tutv'
+
+    return {
+        repo,
+        owner
+    }
+}
+
 const _register = async () => {
     console.log('Register cron jobs.')
 
     Schedule.scheduleJob(EVERY_MINUTE, async () => {
-        const owner = 'tutv'
-        const repo = 'tets-whois'
+        const {owner, repo} = _getCurrentContest()
         const issue = require('./workers/issue')
         await issue.fetchIssues({owner, repo})
     })
@@ -14,9 +23,11 @@ const _register = async () => {
 
 const _run = async () => {
     await _register()
+    const {owner, repo} = _getCurrentContest()
 
-    const owner = 'tutv'
-    const repo = 'tets-whois'
+    console.log('Owner', owner)
+    console.log('Repo', repo)
+
     const issue = require('./workers/issue')
     await issue.fetchIssues({owner, repo})
 
