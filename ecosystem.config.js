2c98273b2db0e6ae8d84574e3a8e12cc28850264
Minh Nguyen <tienminh.uet@gmail.com>
2019-04-13 11:53:44 +0700

    minor

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,22 @@
 module.exports = {
-	apps : [
-	{
-		name: 'whois-api',
-		script: 'index.js',
-		instances: 1,
-		autorestart: true,
-		env: {
-		  NODE_ENV: 'production'
+	apps: [
+		{
+			name: 'whois-api',
+			script: 'index.js',
+			instances: 1,
+			autorestart: true,
+			env: {
+				NODE_ENV: 'production'
+			}
+		},
+		{
+			name: 'whois-worker',
+			script: 'worker.js',
+			instances: 1,
+			autorestart: true,
+			env: {
+				NODE_ENV: 'production'
+			}
 		}
-	},
-	{
-		name: 'whois-worker',
-		script: 'worker.js',
-		instances: 1,
-		autorestart: true,
-		env: {
-		  NODE_ENV: 'production'
-		}
-	}
 	]
 };
